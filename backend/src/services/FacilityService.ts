3c8a9af91957ca1223c3fb8c9bd5dc973c5ddaaf
Viktor Ekström <herrviktor@hotmail.com>
2025-10-23 09:45:21 +0200

    add service functions for getting facilities data from database in FacilityService

---
+++ backend/src/services/FacilityService.ts
@@ -0,0 +1,37 @@
+import FacilityModel from "../models/FacilityModel"
+
+const getFacilityById = async (id: string) => {
+    const facility = await FacilityModel.findById(id);
+    if (!facility) {
+        throw new Error("Inga sporthallar hittade");
+    }
+    return facility;
+};
+
+const getFirstFacility = async () => {
+    const facility = await FacilityModel.findOne();
+    if (!facility) {
+        throw new Error("Ingen sporthall hittades");
+    }
+    return facility;
+};
+
+const getTimeslotsForDate = async (id: string, date: Date) => {
+    const facility = await FacilityModel.findById(id);
+    if (!facility) {
+        throw new Error("Sporthallen hittades ej");
+    }
+
+    const dateObj = facility.availableDates.find( 
+        d => d.date.toISOString().slice(0,10) === date.toISOString().slice(0,10)
+    );
+    if (!dateObj) return [];
+
+    return dateObj.timeslots;
+};
+
+export {
+    getFacilityById,
+    getFirstFacility,
+    getTimeslotsForDate,
+}
